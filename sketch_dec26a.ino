0f26e15c3dab3995b33beeee33fe06944a1b5572
vedrc <54918727+vedrc@users.noreply.github.com>
2022-12-30 13:22:48 -0500

    Accelerometer Experiment #1
    Simplified code to retrieve just accelerometer data, unprocessed.

--- sketch_dec26a.ino
+++ sketch_dec26a.ino
@@ -1,52 +1,50 @@
 #include <Wire.h>
 
-int wireAddress = byte(0x68);
-int X,Y,Z;
+// declaring vars and address of mpu
+int wireAddress = 0x68;
+float accX,accY,accZ;
 
 void setup() {
   Wire.begin(wireAddress);
   Serial.begin(9600);
+
+  // setting up wire measurements with i2c initiation
+  Wire.beginTransmission(wireAddress);
+  Wire.write(0x6B); // PWR_MGMT_1
+  Wire.write(0x00);  // reset
+  Wire.endTransmission(true);
   delay(100);
 
-  // setting up wire measurements
-  Wire.beginTransmission(wireAddress);
-  Wire.write(byte(0x6B));
-  Wire.write(8);  
-  Wire.endTransmission();
-
+  
 }
 
 void loop() {
   // sending values
   Wire.beginTransmission(wireAddress);
-  Wire.write(67); // GYRO_XOUT_H
-  Wire.write(69); // GYRO_YOUT_H
-  Wire.write(71); // GYRO_ZOUT_H
-  Wire.endTransmission();
+  Wire.write(0x3B); // ACCEL_XOUT[15:8]
+  Wire.endTransmission(false);
 
 
-  Wire.requestFrom(wireAddress, 32);
+  Wire.requestFrom(wireAddress, 6, true); // reading accelerometer data (next 6 registers)
 
-  Serial.print(Wire.available()); // still need to figure out why this returns 32 bits when im only sending 24 (8*3)
-  Serial.print("\n");
-
-  // retrieving values
-  if (Wire.available() <= 32) {
-    X = Wire.read();
-    Y = Wire.read();
-    Z = Wire.read();
-  }
+  // retrieving values - shifting 15:8 over 8 bits to convert back to original number
+  accX = (Wire.read() << 8 | Wire.read()) / 16384.0; // 16384.0 = sensitivity
+  accY = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  accZ = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  
 
   // printing values
   Serial.print("X: ");
-  Serial.print(X);
+  Serial.print(accX);
   Serial.print(" ");
   Serial.print("Y: ");
-  Serial.print(Y);
+  Serial.print(accY);
   Serial.print(" ");
   Serial.print("Z: ");
-  Serial.print(Z);
+  Serial.print(accZ);
   Serial.print(" ");
   Serial.print("\n");
+
+  delay(500);
 
 }
