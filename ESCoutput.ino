557854ed09c97d4fe16e6f688722df06dfd263ea
vedrc <54918727+vedrc@users.noreply.github.com>
2023-02-11 21:26:59 -0500

    Dual fan coding for yaw impact
    Updated code to influence both fans (not just one) with opposite values.

--- ESCoutput.ino
+++ ESCoutput.ino
@@ -2,23 +2,25 @@
 #define thrustPin 3 // thrust channel connected to pin 2
 #define yawPin 2 // yaw channel connected to pin 3
 
-byte servoPin = 9; // esc connected to pin 9
-Servo servo; // establishing servo connection
-int thrustValue,yawValue,totalValue;
+byte ccwPin = 9; // esc connected to pin 9
+byte cwPin = 10;
+Servo servoCcw;
+Servo servoCw; // establishing servo connection
+int thrustValue,yawValue,totalValueCw,totalValueCcw;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(thrustPin, INPUT); // beginning reciever transmission
   pinMode(yawPin, INPUT);
-  servo.attach(servoPin); // getting esc connection started 
-  servo.writeMicroseconds(1000); // reset (nothing)
+  servoCw.attach(cwPin); // getting esc connection started
+  servoCcw.attach(ccwPin); 
+  servoCw.writeMicroseconds(1000); // reset (nothing)
+  servoCcw.writeMicroseconds(1000);
   delay(7000); // delay for ESC to startup fully
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   thrustValue = pulseIn(thrustPin, HIGH); // reading values from PWM reciever
   yawValue = pulseIn(yawPin, HIGH);
   
@@ -27,9 +29,12 @@ void loop() {
     Serial.println("not valid");
   }
   else {
-    totalValue = thrustValue + ((yawValue - 1500)/4); // combining yaw and thrust - yaw changes total value by factor of 1/4
-    Serial.println(totalValue);
-    servo.writeMicroseconds(totalValue); // send signal to ESC of whatever RC is at 
+    totalValueCw = thrustValue - ((yawValue - 1500)/4); // combining yaw and thrust - yaw changes total value by factor of 1/4
+    totalValueCcw = thrustValue + ((yawValue - 1500)/4); // combining yaw and thrust - yaw changes total value by factor of 1/4
+    Serial.println(totalValueCw);
+    Serial.println(totalValueCcw);
+    servoCcw.writeMicroseconds(totalValueCcw); // send signal to ESC of whatever RC is at 
+    servoCw.writeMicroseconds(totalValueCw);
 
 
 
